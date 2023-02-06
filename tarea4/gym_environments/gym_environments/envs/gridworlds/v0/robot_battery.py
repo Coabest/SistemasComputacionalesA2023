95237c97c2ea73c5f9e50dac5c6c4f5be2b48aef
Coabest <coalbert_1@hotmail.com>
2023-02-05 23:31:34 -0400

    Adding assets, settings, tilemap and world

--- tarea4/gym_environments/gym_environments/envs/gridworlds/v0/robot_battery.py
+++ tarea4/gym_environments/gym_environments/envs/gridworlds/v0/robot_battery.py
@@ -3,7 +3,11 @@ import time
 import numpy as np
 
 import gym
+from gym import spaces
+import pygame
 
+from . import settingss
+from .world import World
 
 class RobotBatteryEnv(gym.Env):
 
@@ -11,13 +15,25 @@ class RobotBatteryEnv(gym.Env):
         super().__init__()
         self.action_space = gym.spaces.Discrete(4)
         self.observation_space = gym.spaces.Discrete(6)
+        self.current_action = 1
+        self.current_state = 0
+        self.current_reward = 0.0
+        self.delay = settingss.DEFAULT_DELAY
+        self.battery = settingss.BATTERY
         self.P = {0: {0: [(1, 0, 0.0, False)], 1: [(1, 2, 0.0, False)], 2: [(1, 1, 0.0, False)], 3: [(1, 0, 0.0, False)]},
                   1: {0: [(1, 0, 0.0, False)], 1: [(1, 3, 0.0, False)], 2: [(1, 1, 0.0, False)], 3: [(1, 1, 0.0, False)]},
                   2: {0: [(1, 2, 0.0, False)], 1: [(1, 4, 0.0, False)], 2: [(1, 3, 0.0, False)], 3: [(1, 0, 0.0, False)]},
-                  3: {0: [(1, 2, 0.0, False)], 1: [(1, 5, 1.0, True)], 2: [(1, 3, 0.0, False)], 3: [(1, 1, 0.0, False)]},
-                  4: {0: [(1, 4, 0.0, False)], 1: [(1, 4, 0.0, False)], 2: [(1, 5, 1.0, True)], 3: [(1, 2, 0.0, False)]},
-                  5: {0: [(1, 5, 0.0, True)], 1: [(1, 5, 0.0, True)], 2: [(1, 5, 0.0, True)], 3: [(1, 5, 0.0, True)]}}
+                  3: {0: [(1, 2, 0.0, False)], 1: [(1, 5, 1.0,  True)], 2: [(1, 3, 0.0, False)], 3: [(1, 1, 0.0, False)]},
+                  4: {0: [(1, 4, 0.0, False)], 1: [(1, 4, 0.0, False)], 2: [(1, 5, 1.0,  True)], 3: [(1, 2, 0.0, False)]},
+                  5: {0: [(1, 5, 0.0,  True)], 1: [(1, 5, 0.0,  True)], 2: [(1, 5, 0.0,  True)], 3: [(1, 5, 0.0,  True)]}}
         self.reset()
+        self.world = World(
+            "Robot Battery Environment",
+            self.current_state,
+            self.current_action,
+            self.P,
+            self.battery
+        )
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
@@ -27,15 +43,30 @@ class RobotBatteryEnv(gym.Env):
         return self.state, {}
 
     def step(self, action):
-        self.action = action
-        self.reward = self.P[self.state][action][0][2]
-        terminated = self.P[self.state][action][0][3]
-        self.state = self.P[self.state][action][0][1]
+        self.battery -= 1
+        self.action = self.current_action = action
+
+        if np.random.random() < 1 - self.battery / settingss.BATTERY:
+            # Elegir ir a una diferente a la elegida por la acción
+            self.action = self.current_action = (action + 1)%settingss.NUM_ACTIONS
+        
+        self.reward = self.current_reward = self.P[self.current_state][action][0][2]
+        terminated = self.P[self.current_state][action][0][3]
+        self.state = self.current_state = self.P[self.current_state][action][0][1]        
+
+        self.world.update(
+            self.current_state,
+            self.current_action,
+            self.current_reward,
+            terminated
+        )
+        
         self.render()
         time.sleep(0.25)
         return self.state, self.reward, terminated, False, {}
 
     def render(self):
+        self.world.render()
         print(
             "Action {}, reward {}, state {}".format(
                 self.action,
