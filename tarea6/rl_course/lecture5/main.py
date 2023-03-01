cc6b9486bb1652ee6eb03c8f21575c89310506f0
Coabest <coalbert_1@hotmail.com>
2023-03-01 13:24:53 -0400

    adding Q vs QQ comparison

--- tarea6/rl_course/lecture5/main.py
+++ tarea6/rl_course/lecture5/main.py
@@ -2,27 +2,36 @@ import sys
 import time
 import gym
 import gym_environments
-from agent import DoubleQLearning
+from agent import DoubleQLearning, QLearning
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def train(env, agent, episodes):
+    table = []
+
     for episode in range(episodes):
-        print(episode)
+        y = 0
+
         observation, _ = env.reset()
         terminated, truncated = False, False
         while not (terminated or truncated):
             action = agent.get_action(observation, "epsilon-greedy")
             new_observation, reward, terminated, truncated, _ = env.step(action)
+            y += reward
             agent.update(observation, action, new_observation, reward, terminated)
             observation = new_observation
-
+        table.append(y)
+    
+    return table
 
 def play(env, agent):
     observation, _ = env.reset()
     terminated, truncated = False, False
 
     env.render()
-    time.sleep(2)
+    time.sleep(1)
 
     while not (terminated or truncated):
         action = agent.get_action(observation, "greedy")
@@ -37,14 +46,38 @@ if __name__ == "__main__":
     episodes = 10000 if len(sys.argv) < 3 else int(sys.argv[2])
 
     env = gym.make(environments[id])
-    agent = DoubleQLearning(
-        env.observation_space.n, env.action_space.n, alpha=0.1, gamma=0.9, epsilon=0.5
-    )
 
-    train(env, agent, episodes)
-    agent.render()
-    env.close()
+    Q_rewards  = np.zeros(episodes)
+    QQ_rewards = np.zeros(episodes)
+    tests = 500
 
-    env = gym.make(environments[id], render_mode="human")
-    play(env, agent)
+    for i in range(tests):
+        Q_agent = QLearning(
+            env.observation_space.n, env.action_space.n, alpha=0.1, gamma=0.9, epsilon=0.3
+        )
+        QQ_agent = DoubleQLearning(
+            env.observation_space.n, env.action_space.n, alpha=0.1, gamma=0.9, epsilon=0.3
+        )
+        if (i%(int(tests/10 + 1))==0):
+            print("test #{}".format(i))
+        Q_table  = train(env,  Q_agent, episodes)
+        QQ_table = train(env, QQ_agent, episodes)
+        for episode in range(episodes):
+            Q_rewards[episode]  += Q_table[episode]
+            QQ_rewards[episode] += QQ_table[episode]
+
+    for episode in range(episodes):
+        Q_rewards[episode]  /= tests
+        QQ_rewards[episode] /= tests
+
+    plt.plot(Q_rewards, label = 'QLearning')
+    plt.plot(QQ_rewards, label = 'QQLearning')
+    plt.xlabel('x - Episode')
+    plt.ylabel('y - Reward')
+    
+    plt.title('QLearning Vs. Double QLearning')
+    plt.legend()
+    plt.show()
+
+    QQ_agent.render()
     env.close()
