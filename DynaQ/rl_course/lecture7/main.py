6c69c01afa452d8ea0c21895dc1bfd3fb834173d
Coabest <coalbert_1@hotmail.com>
2023-04-30 13:56:43 -0400

    update

--- DynaQ/rl_course/lecture7/main.py
+++ DynaQ/rl_course/lecture7/main.py
@@ -9,10 +9,9 @@ import matplotlib.pyplot as plt
 
 def run(env, agent, selection_method, episodes):
     steps_per_episode = []
-    for episode in range(episodes):
+    for episode in range(1, episodes+1):
         steps = 0
-        if episode > 0:
-            print(f"Episode: {episode+1}")
+        print(f"Episode: {episode}")
         observation, _ = env.reset()
         agent.start_episode()
         terminated, truncated = False, False
@@ -36,13 +35,18 @@ def run(env, agent, selection_method, episodes):
 if __name__ == "__main__":
     environments = ["Princess-v0", "Blocks-v0"]
     id = 1 if len(sys.argv) < 2 else int(sys.argv[1])
-    episodes = 350 if len(sys.argv) < 3 else int(sys.argv[2])
+    episodes = 7 if len(sys.argv) < 3 else int(sys.argv[2])
 
     env = gym.make(environments[id])
 
     steps_DQ = np.zeros(episodes)
     steps_DQPlus = np.zeros(episodes)
-    runs = 3
+    runs = 1
+
+    bestEpisode_DQ = 1e9
+    bestEpisode_DQPlus = 1e9
+    worstEpisode_DQ = -1
+    worstEpisode_DQPlus = -1
 
     for i in range(runs):
         DQ_agent = DYNAQ(
@@ -52,7 +56,10 @@ if __name__ == "__main__":
             gamma=0.95,
             epsilon=0.1
         )
-        steps_DQ += run(env, DQ_agent, "epsilon-greedy", episodes )
+        DQ_run = run(env, DQ_agent, "epsilon-greedy", episodes )
+        bestEpisode_DQ = np.min(DQ_run)
+        worstEpisode_DQ = np.max(DQ_run)
+        steps_DQ += DQ_run
 
         DQPlus_agent = DYNAQPLUS(
             env.observation_space.n,
@@ -62,10 +69,16 @@ if __name__ == "__main__":
             epsilon=0.1,
             kappa=1e-4
         )
-        steps_DQPlus += run(env, DQPlus_agent, "epsilon-greedy", episodes )
+        DQPLus_run = run(env, DQPlus_agent, "epsilon-greedy", episodes)
+        bestEpisode_DQPlus = np.min(DQPLus_run)
+        worstEpisode_DQPlus = np.max(DQPLus_run)
+        steps_DQPlus += DQPLus_run
     
     steps_DQ = steps_DQ / runs
     steps_DQPlus = steps_DQPlus / runs
+
+    print(f"Best: {bestEpisode_DQ} Worst: {worstEpisode_DQ}  - DynaQ")
+    print(f"Best: {bestEpisode_DQPlus} Worst: {worstEpisode_DQPlus}  - DynaQPlus")
 
     plt.plot(steps_DQ, label = 'Dyna-Q')
     plt.plot(steps_DQPlus, label = 'Dyna-Q+')
@@ -75,5 +88,20 @@ if __name__ == "__main__":
     plt.title('Dyna-Q Vs. Dyna-Q+')
     plt.legend()
     plt.show()
+
+    steps_difference = 0
+    for i in range(episodes):
+        steps_difference += int(steps_DQ[i] - steps_DQPlus[i])
+
+    print(f"\nIn total Dyna-Q+ took {steps_DQPlus} less steps than Dyna-Q")
+
+    plt.plot(steps_DQ - steps_DQPlus, label = 'DynaQPLus - DynaQ')
+    plt.xlabel('x - Episodes')
+    plt.ylabel('y - Diference in steps per episode')
+    
+    plt.title('Dyna-Q Vs. Dyna-Q+ Difference')
+    plt.legend()
+    plt.show()
+
 
     env.close()
